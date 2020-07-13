87bbd966ada88c338f57e9cab64e3b4d099da5ef
Daniel Moura <darthur.moura@gmail.com>
2020-07-13 18:42:37 -0300

    Subindo o exercício 6

---
+++ 4_1/exercicio6.js
@@ -0,0 +1,22 @@
+let peca = "TorRe".toLowerCase();
+
+switch (peca) {
+    case "peao":
+        console.log("O Peão se move 1 quadrado a frente (2, se no começo da partida).\nEle come a peça adversária na diagonal adjacente.");
+        break;
+    case "rainha":
+        console.log("A Rainha se move por todo o tabuleiro em linha reta ou nas diagonais.");
+        break;
+    case "rei":
+        console.log("O Rei se move 1 quadrado em qualquer direção.");
+        break;
+    case "cavalo":
+        console.log("O Cavalo se move em L (2 quadrados em linha reta e 1 quadrado ao lado).");
+        break;
+    case "bispo":
+        console.log("O Bispo se move por todo o tabuleiro nas diagonais.");
+        break;
+    case "torre":
+        console.log("A Torre se move por todo o tabuleiro em linhas retas.");
+        break;
+}
