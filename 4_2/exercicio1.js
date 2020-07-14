882147c38d2e99fc994ae526d63909acc413d7d0
Daniel Moura <darthur.moura@gmail.com>
2020-07-14 15:58:21 -0300

    Exercício 1 > Subindo a parte 3

--- 4_2/exercicio1.js
+++ 4_2/exercicio1.js
@@ -1,6 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let i;
 let somaTodos = 0;
+let mediaArit = somaTodos / numbers.length;
 
 for(i in numbers) {
   console.log(numbers[i]);
@@ -12,3 +13,5 @@ for(i = 0; i < numbers.length; i++) {
 	somaTodos = somaTodos + numbers[i];
 }
 console.log("A soma de todos os itens no array é", somaTodos);
+
+console.log("A média aritmética do array é", mediaArit);
