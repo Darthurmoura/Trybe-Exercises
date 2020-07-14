37b4ce1ceb7f7f90eca3697e7ba08f4d3a359169
Daniel Moura <darthur.moura@gmail.com>
2020-07-14 18:10:54 -0300

    Exercício 1 > Subindo a parte 4

--- 4_2/exercicio1.js
+++ 4_2/exercicio1.js
@@ -1,17 +1,29 @@
+// Configurando variáveis e iterador
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let i;
 let somaTodos = 0;
-let mediaArit = somaTodos / numbers.length;
 
+// Exercício 1
 for(i in numbers) {
   console.log(numbers[i]);
 }
 
 console.log("");
 
+// Exercício 2
 for(i = 0; i < numbers.length; i++) {
 	somaTodos = somaTodos + numbers[i];
 }
 console.log("A soma de todos os itens no array é", somaTodos);
 
+// Exercício 3 
+let mediaArit = somaTodos / numbers.length;
 console.log("A média aritmética do array é", mediaArit);
+
+// Exercício 4
+if(mediaArit > 20) {
+	console.log("A média aritmética é maior que 20.");
+} else {
+	console.log("A média aritmética é menor ou igual a 20.");
+}
+
