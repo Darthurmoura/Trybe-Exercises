8deb8cc9c468f2acfd363ca33ce6bd1fcbfcb257
Daniel Moura <darthur.moura@gmail.com>
2020-07-14 15:55:02 -0300

    Exercício 1 > Subindo a parte 2

--- 4_2/exercicio1.js
+++ 4_2/exercicio1.js
@@ -1,6 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let i;
+let somaTodos = 0;
 
 for(i in numbers) {
-    console.log(numbers[i]);
+  console.log(numbers[i]);
 }
+
+console.log("");
+
+for(i = 0; i < numbers.length; i++) {
+	somaTodos = somaTodos + numbers[i];
+}
+console.log("A soma de todos os itens no array é", somaTodos);
