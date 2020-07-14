3a7eb43fef7120a2959cb68ea4f3b7d6121628de
Daniel Moura <darthur.moura@gmail.com>
2020-07-14 14:07:19 -0300

    Otimizando o exercício 8

--- 4_1/exercicio8.js
+++ 4_1/exercicio8.js
@@ -1,12 +1,11 @@
 let a = 9;
 let b = 5;
-let c = 8;
+let c = 7;
 let isEven = false;
 
 
 if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
   isEven = true;
-  console.log(isEven);
-} else {
-  console.log(isEven);
 }
+
+console.log(isEven)
