16145ba311901fb5b51b13e0998ffd4daa619b5c
Daniel Moura <darthur.moura@gmail.com>
2020-07-16 19:14:37 -0300

    Correção do código do exercício 8

--- 4_4/exercicio8_somatorio.js
+++ 4_4/exercicio8_somatorio.js
@@ -1,22 +1,10 @@
-let array = [2, 3, 2, 5, 8, 2, 3];
-let maxFreq = 1;
-let rep = 0;
-let item;
+let soma = 0;
 
-function maisRepetido(array) {
-  for (let i = 0; i < array.length; i += 1) {
-    for (let j = 0; j < array.length; j += 1) {
-      if (array[i] === array[j]) {
-        rep += 1;
-        if (maxFreq < rep) {
-          maxFreq = rep;
-          item = array[i];
-        }
-      }
-    }
-    rep = 0
+function somador(n) {
+  for (let i = 0; i <= n; i += 1) {
+    soma += i;
   }
-  return item
+  return soma;
 }
 
-console.log(maisRepetido(array));
+console.log(somador(5));
