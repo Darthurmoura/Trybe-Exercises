06936bccef2b5037a4fecb62db6c719dd117e9f0
Daniel Moura <darthur.moura@gmail.com>
2020-08-10 19:22:51 -0300

    Exercício 2 - longestWord

---
+++ 7_1/longestWord.js
@@ -0,0 +1,12 @@
+const longestWord = phrase => {
+  let words = phrase.split(' ');
+  let longest = '';
+  for(let i = 0; i < words.length; i += 1) {
+    if(words[i].length > longest.length) {
+      longest = words[i];
+    }
+  }
+  return longest;
+}
+
+console.log(longestWord(`Crie uma função que receba uma frase e retorne qual a maior palavra`));
